402a2150fed31dc6c925f7e4ff152b11432204b9
Danilo Soares <danilo.silva@nutes.ufpe.br>
2022-08-12 10:47:10 -0300

    Atividade05 Móidulo02 de Lógica.

---
+++ LogicaProgOO/Modulo02/Atividade05/atividade05.py
@@ -0,0 +1,19 @@
+import calculadora
+import os
+import time
+num1 = 0.0
+num2 = 0.0
+teste = 5
+while(teste != 0):
+    os.system("cls")
+    teste = int(input("Escolha uma opção: \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \n0 - Sair\n"))
+    if( teste >= 1 and teste <= 4):
+        num1 = float(input("Informe o primeiro número: "))
+        num2 = float(input("Informe o segundo número: "))
+        print("Resultado: ", calculadora.calculadora(num1, num2, teste))
+        time.sleep(3)
+    elif(teste == 0):
+        print("Vlw!!!")
+    else:
+        print("Opção inválida, escolha uma opção valida na próxima!")
+        time.sleep(3)
---
+++ LogicaProgOO/Modulo02/Atividade05/calculadora.py
@@ -0,0 +1,11 @@
+def calculadora(num1, num2, op):
+    if op == 1:
+        return num1 + num2
+    elif op == 2:
+        return num1 - num2
+    elif op == 3:
+        return num1 * num2
+    elif op == 4:
+        return num1 / num2
+    else:
+        return 0
