a54484dd22a6096a40710f047fded6d7ab0f050a
Danilo Soares <danilo.silva@nutes.ufpe.br>
2022-09-30 12:03:32 -0300

    Introdução a JS

---
+++ JS/Modulo00/curriculo/js/paulo.js
@@ -0,0 +1,6 @@
+function mudarCurriculo(){
+    var titulo = document.getElementById("titulo");
+    titulo.innerHTML = "Paulo César Ribeiro da Silva";
+    var foto = document.getElementById("foto");
+    foto.src = "img/paulo.jfif";
+}
---
+++ JS/Modulo00/js/segundaPag.js
@@ -0,0 +1,5 @@
+document.write("<h3>", document.title,"</h3>");
+document.write("Abel: Oi estou bem!");
+function titulosite() {
+    alert("Você clicou no botão com JS externo!");    
+}
